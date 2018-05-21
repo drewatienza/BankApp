6d834fef07e868b1ba49375ce4565e4d93564831
drewatienza <atienza.drew@gmail.com>
2018-05-21 14:14:27 -0700

    created the outline for all bank accounts and interest rate

--- src/IBaseRate.java
+++ src/IBaseRate.java
@@ -1,3 +1,4 @@
 public interface IBaseRate {
 
+    // Write a method that returns the base rate
 }
--- src/Savings.java
+++ src/Savings.java
@@ -1,3 +1,7 @@
 public class Savings extends Account {
+    // List properties specific to savings account
 
+    // Constructor to initialize settings for the savings account
+
+    // List any methods specific to savings account
 }
