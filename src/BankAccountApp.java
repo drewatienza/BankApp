631f1e0c66383382805f6991994e143d772dd2be
drewatienza <atienza.drew@gmail.com>
2018-05-23 14:39:38 -0700

    implemented using super constructors with inheritance

--- src/BankAccountApp.java
+++ src/BankAccountApp.java
@@ -2,6 +2,12 @@ public class BankAccountApp {
 
     public static void main(String[] args) {
 
+        Checking chkacc1 = new Checking("Tom Wilson");
+
+        Savings savacc1 = new Savings("Rich Lowe");
+
+
+        // Read a CSV File then create new accounts based on that data
     }
 
 }
--- src/Savings.java
+++ src/Savings.java
@@ -2,6 +2,10 @@ public class Savings extends Account {
     // List properties specific to savings account
 
     // Constructor to initialize settings for the savings account
+    public Savings(String name) {
+        super(name);
+        System.out.println("NEW SAVINGS ACCOUNT");
+    }
 
     // List any methods specific to savings account
 }
