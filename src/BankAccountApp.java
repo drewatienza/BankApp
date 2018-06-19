154bd1d542dba5b7dfeb99c3633e937d712d77f7
drewatienza <atienza.drew@gmail.com>
2018-06-19 15:50:13 -0700

    created a list of objects and finished project

--- src/BankAccountApp.java
+++ src/BankAccountApp.java
@@ -1,38 +1,33 @@
 import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
 
 public class BankAccountApp {
 
     public static void main(String[] args) throws IOException {
-        String file = "/Users/drewatienza/Downloads/NewBankAccounts.csv";
-
-//        Checking chkacc1 = new Checking("Tom Wilson", "635928863", 1500);
-//        Savings savacc1 = new Savings("Rich Lowe", "374885228", 2500);
-//
-//        savacc1.compound();
-//        System.out.println("********************");
-//
-//        savacc1.showInfo();
-//        System.out.println("********************");
-//        chkacc1.showInfo();
-//        System.out.println("********************");
+        List<Account> accounts = new LinkedList<Account>();
 
 
         // Read a CSV File then create new accounts based on that data
+        String file = "/Users/drewatienza/Desktop/JavaPrograms/BankApp/src/NewBankAccounts.csv";
         List<String[]> newAccountHolders = utilities.CSV.read(file);
         for (String[] accountHolder : newAccountHolders) {
             String name = accountHolder[0];
             String sSN = accountHolder[1];
             String accountType = accountHolder[2];
             double initDeposit = Double.parseDouble(accountHolder[3]);
-//            System.out.println(name + " " + sSN + " " + accountType + " $" + initDeposit);
             if ( accountType.equals("Savings")) {
-                System.out.println("OPEN A SAVINGS ACCOUNT");
+                accounts.add(new Savings(name, sSN, initDeposit));
             } else if (accountType.equals("Checking")) {
-                System.out.println("OPEN A CHECKING ACCOUNT");
+                accounts.add(new Checking(name, sSN, initDeposit));
             } else {
                 System.out.println("ERROR READING ACCOUNT TYPE");
             }
+        }
+
+        for (Account acc : accounts) {
+            System.out.println("\n********************");
+            acc.showInfo();
         }
 
     }
